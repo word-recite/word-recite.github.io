a5c8dea965a4d9a1d5cbd5b6111fccf71bc363cf
word-recite <98540561+word-recite@users.noreply.github.com>
2022-07-07 17:00:02 +0800

    remove unused keepaccounts content

---
+++ version.js
@@ -0,0 +1,10 @@
+fetch('/version.json')
+     .then(function (response) {
+            return response.json();
+     })
+     .then(function (data) {
+          for (var app in data){
+              var name =app.split(".")[2]
+              document.getElementById(name).innerHTML = data[app]["iOS"]
+          }
+     })
